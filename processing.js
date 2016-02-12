651a6aef32deac03bed235366659dc1834fd6768
Thibaud Colas <thibaudcolas@gmail.com>
2016-02-12 15:16:10 +0200

    Add basic processing to get a weekly commit chart

--- processing.js
+++ processing.js
@@ -22,3 +22,24 @@ ownRepos.forEach((repo) => {
 const totalActivity = ownRepos.reduce((sum, repo) => sum + repo.totalActivity, 0);
 
 console.log('total', totalActivity);
+
+const commitActivity = ownRepos.reduce((total, repo) => {
+    repo.commitActivity.forEach((week) => {
+        const weekTimestamp = (Math.floor(week.week / 500000) * 500000) * 1000;
+
+        total[weekTimestamp] = (total[weekTimestamp] || 0) + week.total;
+    });
+
+    return total;
+}, {});
+
+const commitActivityArray = Object.keys(commitActivity).map((key) => {
+    return {
+        date: parseInt(key, 10),
+        value: commitActivity[key],
+    };
+});
+
+console.log(commitActivityArray);
+console.log(commitActivityArray.reduce((sum, week) => sum + week.value, 0));
+console.log(commitActivityArray.length);
